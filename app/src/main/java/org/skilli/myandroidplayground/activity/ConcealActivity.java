420153e11b1e84e835f4d4f870b21159c1b5da18
_cjae <cjaeomon@gmail.com>
2016-09-22 15:09:06 +0100

    bug fixes

--- app/src/main/java/org/skilli/myandroidplayground/activity/ConcealActivity.java
+++ app/src/main/java/org/skilli/myandroidplayground/activity/ConcealActivity.java
@@ -3,13 +3,16 @@ package org.skilli.myandroidplayground.activity;
 import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.facebook.android.crypto.keychain.SharedPrefsBackedKeyChain;
 import com.facebook.crypto.Crypto;
@@ -20,7 +23,6 @@ import com.facebook.crypto.util.SystemNativeCryptoLibrary;
 
 import org.skilli.myandroidplayground.R;
 import org.skilli.myandroidplayground.util.CryptoTestUtils;
-import org.skilli.myandroidplayground.util.MyKeyChain;
 
 import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
@@ -33,15 +35,20 @@ import java.io.OutputStream;
 
 public class ConcealActivity extends AppCompatActivity {
 
-    final String MEDIA_PATH = new String("/storage/sdcard0/Pictures/Screenshots/newImagefile.txt");
+//    final String MEDIA_PATH = new String("/storage/sdcard0/Pictures/Screenshots/newImagefile.txt");
+//    private File file = new File(MEDIA_PATH);
+//
+//    final String VIDEO_MEDIA_PATH = new String("/storage/sdcard0/Pictures/Screenshots/image.png");
+
+    final String MEDIA_PATH = "/storage/sdcard0/Pictures/Screenshots/newVideoFile.txt";
     private File file = new File(MEDIA_PATH);
 
-    final String PICTURE_MEDIA_PATH = new String("/storage/sdcard0/Pictures/Screenshots/image.png");
-
-    String content = "This is the text content";
+    final String VIDEO_MEDIA_PATH = "/storage/sdcard0/Pictures/Screenshots/threading.mp4";
+    private File videoFile = new File(VIDEO_MEDIA_PATH);
 
     Button decryptBtn;
     ImageView displayImage;
+    VideoView displayVideo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +56,8 @@ public class ConcealActivity extends AppCompatActivity {
         setContentView(R.layout.activity_conceal);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        displayVideo = (VideoView) findViewById(R.id.videoView1);
 
         displayImage = (ImageView) findViewById(R.id.displayImage);
         decryptBtn = (Button) findViewById(R.id.decryptBtn);
@@ -94,9 +103,15 @@ public class ConcealActivity extends AppCompatActivity {
         OutputStream fileStream = new BufferedOutputStream(new FileOutputStream(file));
         OutputStream outputStream = crypto.getCipherOutputStream(fileStream, new Entity(CryptoTestUtils.ENTITY_NAME));
 
-//        byte[] contentInBytes = content.getBytes();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        FileInputStream fis = new FileInputStream(videoFile);
 
-        outputStream.write(bitmapToBytes(convertFilePath(PICTURE_MEDIA_PATH)));
+        byte[] buf = new byte[1024];
+        int n;
+        while (-1 != (n = fis.read(buf)))
+            baos.write(buf, 0, n);
+
+        outputStream.write(baos.toByteArray());
         outputStream.close();
 
         progDailog.dismiss();
@@ -134,35 +149,116 @@ public class ConcealActivity extends AppCompatActivity {
 
         inputStream.close();
 
-        Bitmap bitmap = bytesToBitmap(out.toByteArray());
-
         progDailog.dismiss();
 
-        displayImage.setImageBitmap(bitmap);
+        File outputDir = getApplicationContext().getCacheDir();
+        String VIDEO_MEDIA_PATH = "decThreading";
+        File tempDecVideoFile = File.createTempFile(VIDEO_MEDIA_PATH, ".mp4", outputDir);
+        tempDecVideoFile.deleteOnExit();
 
-//        String s = new String(String.valueOf(out));
-//        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+        FileOutputStream fos = new FileOutputStream(tempDecVideoFile);
+        fos.write(out.toByteArray());
+        fos.close();
+
+        displayVideo.setVideoURI((Uri.fromFile(tempDecVideoFile)));
+        displayVideo.setMediaController(new MediaController(this));
+        displayVideo.start();
+        displayVideo.requestFocus();
     }
 
-    private Bitmap convertFilePath(String photoPath){
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-        Bitmap bitmap = BitmapFactory.decodeFile(photoPath, options);
+//    private void doEncrypt() throws IOException, KeyChainException, CryptoInitializationException {
+//
+//        ProgressDialog progDailog = ProgressDialog.show(this, "Encrypting", "Please wait...", true);
+//
+//        Crypto crypto = new Crypto(
+//                new SharedPrefsBackedKeyChain(this),
+//                new SystemNativeCryptoLibrary());
+//
+//        if (!crypto.isAvailable()) {
+//            Toast.makeText(getApplicationContext(), "Crypto not available", Toast.LENGTH_SHORT).show();
+//            return;
+//        }
+//
+//        OutputStream fileStream = new BufferedOutputStream(new FileOutputStream(videoFile));
+//        OutputStream outputStream = crypto.getCipherOutputStream(fileStream, new Entity(CryptoTestUtils.ENTITY_NAME));
+//
+////        byte[] contentInBytes = content.getBytes();
+//
+//        outputStream.write(bitmapToBytes(convertFilePath(VIDEO_MEDIA_PATH)));
+//        outputStream.close();
+//
+//        progDailog.dismiss();
+//    }
+//
+//    private void doDecrypt() throws IOException, KeyChainException, CryptoInitializationException {
+//
+//        ProgressDialog progDailog = ProgressDialog.show(this, "Decrypting", "Please wait...", true);
+//
+////        Crypto crypto = new Crypto(
+////                new MyKeyChain(this,"bmd1eWVudGllbmxvbmc="),
+////                new SystemNativeCryptoLibrary());
+//
+//        Crypto crypto = new Crypto(
+//                new SharedPrefsBackedKeyChain(this),
+//                new SystemNativeCryptoLibrary());
+//
+//        if (!crypto.isAvailable()) {
+//            Toast.makeText(getApplicationContext(), "Crypto not available", Toast.LENGTH_SHORT).show();
+//            return;
+//        }
+//
+//        FileInputStream fileStream = new FileInputStream(file);
+//
+//        InputStream inputStream = crypto.getCipherInputStream(
+//                fileStream, new Entity(CryptoTestUtils.ENTITY_NAME));
+//
+//        int read;
+//        byte[] buffer = new byte[1024];
+//        ByteArrayOutputStream out = new ByteArrayOutputStream();
+//
+//        while ((read = inputStream.read(buffer)) != -1) {
+//            out.write(buffer, 0, read);
+//        }
+//
+//        inputStream.close();
+//
+//        Bitmap bitmap = bytesToBitmap(out.toByteArray());
+//
+//        progDailog.dismiss();
+//
+//        displayImage.setImageBitmap(bitmap);
+//
+////        String s = new String(String.valueOf(out));
+////        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+//    }
 
-        return bitmap;
-    }
-
-    // convert Bitmap to bytes
-    private byte[] bitmapToBytes(Bitmap photo) {
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        photo.compress(Bitmap.CompressFormat.PNG, 100, stream);
-        byte[] byteArray = stream.toByteArray();
-        return byteArray;
-    }
-
-    // convert bytes to Bitmap
-    private Bitmap bytesToBitmap(byte[] bytes) {
-        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-        return bitmap;
-    }
+//    private Bitmap convertFilePath(String photoPath){
+//        BitmapFactory.Options options = new BitmapFactory.Options();
+//        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+//        Bitmap bitmap = BitmapFactory.decodeFile(photoPath, options);
+//
+//        return bitmap;
+//    }
+//
+////    private Bitmap convertFilePath(String photoPath){
+////        BitmapFactory.Options options = new BitmapFactory.Options();
+////        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+////        Bitmap bitmap = BitmapFactory.decodeFile(photoPath, options);
+////
+////        return bitmap;
+////    }
+//
+//    // convert Bitmap to bytes
+//    private byte[] bitmapToBytes(Bitmap photo) {
+//        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+//        photo.compress(Bitmap.CompressFormat.PNG, 100, stream);
+//        byte[] byteArray = stream.toByteArray();
+//        return byteArray;
+//    }
+//
+//    // convert bytes to Bitmap
+//    private Bitmap bytesToBitmap(byte[] bytes) {
+//        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+//        return bitmap;
+//    }
 }
--- app/src/main/java/org/skilli/myandroidplayground/activity/MainActivity.java
+++ app/src/main/java/org/skilli/myandroidplayground/activity/MainActivity.java
@@ -5,7 +5,14 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
 import org.skilli.myandroidplayground.R;
 import org.skilli.myandroidplayground.databinding.MainActivityBinding;
@@ -15,6 +22,8 @@ import org.skilli.myandroidplayground.model.User;
 public class MainActivity extends AppCompatActivity {
 
     private MainActivityBinding binding;
+    private TextSwitcher mSwitcher;
+    private int mCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +35,30 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         setUpUI();
+        setTextswitcher();
+    }
+
+    private void setTextswitcher() {
+        mSwitcher = (TextSwitcher) findViewById(R.id.switcher);
+
+        mSwitcher.setFactory(mFactory);
+
+        Animation in = AnimationUtils.loadAnimation(this,
+                android.R.anim.fade_in);
+        Animation out = AnimationUtils.loadAnimation(this,
+                android.R.anim.fade_out);
+        mSwitcher.setInAnimation(in);
+        mSwitcher.setOutAnimation(out);
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCounter++;
+                mSwitcher.setText(String.valueOf(mCounter));
+            }
+        });
+
+        mSwitcher.setCurrentText(String.valueOf(mCounter));
     }
 
     private void setUpUI() {
@@ -38,5 +71,25 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, ConcealActivity.class));
             }
         });
+
+        binding.buttonReveal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ConcealActivity.class));
+            }
+        });
     }
+
+    private ViewSwitcher.ViewFactory mFactory = new ViewSwitcher.ViewFactory() {
+
+        @Override
+        public View makeView() {
+
+            // Create a new TextView
+            TextView t = new TextView(MainActivity.this);
+            t.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL);
+            t.setTextAppearance(MainActivity.this, android.R.style.TextAppearance_Large);
+            return t;
+        }
+    };
 }
